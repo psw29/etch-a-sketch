5ea5ee7a139fa1066f6b272bf5a5de43890a0e48
psw29 <pkjsw1992@gmail.com>
2015-05-24 22:39:26 +0100

    added colour choice

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-    alert($('.container').length);
     var rows = 50;
     var cols = 50;
     var number_of_squares = rows * cols;
@@ -9,6 +8,7 @@ $(document).ready(function(){
     for (var i = 0; i < number_of_squares; i++) {
         $('.container').append('<div class="square"></div>');
     }
+
     $(".square").css({
         'height': '10px',
         'width': '10px',
@@ -17,13 +17,32 @@ $(document).ready(function(){
         'float': 'left'
     });
 
-    $(".square").on('mouseenter', function() {
-        $(this).addClass('highlighted');
+    $('#black').on('click', function() {
+        $(".square").on('mouseenter', function() {
+            $(this).css('background-color', '#111828')
+        });
     });
 
-    $("button").on('click', function() {
-        $(".square").removeClass('highlighted');
+    $('#red').on('click', function() {
+        $(".square").on('mouseenter', function() {
+            $(this).css('background-color', '#ff4444')
+        });
     });
 
+    $('#blue').on('click', function() {
+        $(".square").on('mouseenter', function() {
+            $(this).css('background-color', '#509ddd')
+        });
+    });
+
+    $('#green').on('click', function() {
+        $(".square").on('mouseenter', function() {
+            $(this).css('background-color', '#5ca36a')
+        });
+    });
+
+    $(".reset").on('click', function() {
+        $(".square").css('background-color', 'transparent');
+    });
 
 });
